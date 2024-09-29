5ef5a197394018484f2a6d7ec5ef612d853c17b4
OscartGiles <oscart.giles@gmail.com>
2024-09-29 17:46:39 +0100

    Remove CLI comment about dates not being validated

--- airbend-ingest/src/cli.rs
+++ airbend-ingest/src/cli.rs
@@ -4,11 +4,11 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 /// Welcome to the airbend-ingest tool. Query the London Air Quality Network API (LAQN) and ingest into a databend database.
 pub struct Cli {
-    /// Start date to request data from. Ensure it is format 'yyyy-mm-dd'. This is not validated.
+    /// Start date to request data from. Ensure it is format 'yyyy-mm-dd'.
     #[arg(short, long)]
     pub start_date: String,
 
-    /// End date to request data from. Ensure it is format 'yyyy-mm-dd'. This is not validated.
+    /// End date to request data from. Ensure it is format 'yyyy-mm-dd'.
     #[arg(short, long)]
     pub end_date: String,
 
